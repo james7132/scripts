2f338f7db605affd1ef745b97a44fd196816995a
james7132 <james.siqi.liu@gmail.com>
2018-10-29 07:24:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# james7132's shell scripts
+
+A variety of shell scripts for a variety of utilities. Largely built to work with
+bash.
+
+## Scripts
+
+ * `sqlite_backup` - Backs up sqlite databases, gzips them, then saves them in a
+   backup Backblaze B2 bucket. Requires the B2 CLI to be installed (requires
+   Python 2/3 and pip)
---
+++ sqlite_backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Usage: sqlite_backup.sh <sqlite dbs>...
+#   1. Creates a backup of of each of the SQlite databases
+#      provided.
+#   2. Gzips the backed up database.
+#   3. Uploads the gzipped backup to a Backblaze bucket.
+
+DATE=`date +"%Y%m%d"`
+for DB_FILE in "$@"
+do
+  BACK_FILE="$DB_FILE.$DATE"
+  echo "Backing up $DB_FILE..."
+  sqlite3 $DB_FILE ".backup $BACK_FILE" &
+done
+
+wait
+
+for DB_FILE in "$@"
+do
+  BACK_FILE="$DB_FILE.$DATE"
+  GZIP_FILE="$BACK_FILE.gz"
+  echo "Compressing $BACK_FILE..."
+  gzip -9 $BACK_FILE
+  echo "Backing up to Backblaze"
+  b2 upload-file james-db-backup "$GZIP_FILE" "$DB_FILE/$GZIP_FILE" > /dev/null
+  rm $GZIP_FILE
+done

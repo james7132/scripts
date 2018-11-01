f9ee175f73737ce5d238ff9b159f6e291464fe3d
James Liu <james.siqi.liu@gmail.com>
2018-11-01 18:33:46 +0000

    Log to syslog

--- sqlite_backup.sh
+++ sqlite_backup.sh
@@ -6,11 +6,13 @@
 #   2. Gzips the backed up database.
 #   3. Uploads the gzipped backup to a Backblaze bucket.
 
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+
 DATE=`date +"%Y%m%d"`
 for DB_FILE in "$@"
 do
   BACK_FILE="$DB_FILE.$DATE"
-  echo "Backing up $DB_FILE..."
+  echo "Backing up $DB_FILE..." | logger
   sqlite3 $DB_FILE ".backup $BACK_FILE" &
 done
 
@@ -21,7 +23,7 @@ do
   BACK_FILE="$DB_FILE.$DATE"
   GZIP_FILE="$BACK_FILE.gz"
   BACKBLAZE_FILE="$(basename $DB_FILE)/$(basename $GZIP_FILE)"
-  echo "Vacuuming $BACK_FILE..."
+  echo "Vacuuming $BACK_FILE..."  
   sqlite3 $DB_FILE "vacuum;"
   echo "Compressing $BACK_FILE..."
   gzip -9 $BACK_FILE
